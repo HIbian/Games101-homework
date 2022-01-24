5831462a37e3f3c54e758bff4402c49c1c50f7dc
hibian-arch <416711267@qq.com>
2022-01-24 17:07:38 +0800

    实现displacement

--- Assignment3/Code/main.cpp
+++ Assignment3/Code/main.cpp
@@ -237,23 +237,51 @@ Eigen::Vector3f displacement_fragment_shader(const fragment_shader_payload &payl
     float kh = 0.2, kn = 0.1;
 
     // TODO: Implement displacement mapping here
-    // Let n = normal = (x, y, z)
-    // Vector t = (x*y/sqrt(x*x+z*z),sqrt(x*x+z*z),z*y/sqrt(x*x+z*z))
-    // Vector b = n cross product t
-    // Matrix TBN = [t b n]
-    // dU = kh * kn * (h(u+1/w,v)-h(u,v))
-    // dV = kh * kn * (h(u,v+1/h)-h(u,v))
-    // Vector ln = (-dU, -dV, 1)
-    // Position p = p + kn * n * h(u,v)
-    // Normal n = normalize(TBN * ln)
+    Eigen::Vector3f n = normal;
+    Eigen::Vector3f t(n.x() * n.y() / std::sqrt(n.x() * n.x() + n.z() * n.z()),
+                      std::sqrt(n.x() * n.x() + n.z() * n.z()),
+                      n.z() * n.y() / std::sqrt(n.x() * n.x() + n.z() * n.z()));
+    Eigen::Vector3f b = n.cross(t);
+    //坐标系变换,推导 http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-13-normal-mapping/
+    Eigen::Matrix3f TBN;
+    TBN << t, b, n;
+    float u = payload.tex_coords[0];
+    float v = payload.tex_coords[1];
+    float w = payload.texture->width;
+    float h = payload.texture->height;
+    //贴图提供的法线信息,norm()可以看作高度,转换为以为坐标.https://blog.csdn.net/m0_56348460/article/details/117386857
+    float dU = kh * kn * (payload.texture->getColor(u + 1 / w, v).norm() - payload.texture->getColor(u, v).norm());
+    float dV = kh * kn * (payload.texture->getColor(u, v + 1 / h).norm() - payload.texture->getColor(u, v).norm());
+    Vector3f ln(-dU,-dV,1);
+    normal = (TBN * ln).normalized();
 
+    // Position p = p + kn * n * h(u,v)
+    point = point + kn * n * payload.texture->getColor(u,v).norm();
 
     Eigen::Vector3f result_color = {0, 0, 0};
-
     for (auto &light: lights) {
         // TODO: For each light source in the code, calculate what the *ambient*, *diffuse*, and *specular* 
         // components are. Then, accumulate that result on the *result_color* object.
+        //get l_vect,n_vec,v_vec,r
+        Eigen::Vector3f l_vect = light.position - point;
+        Eigen::Vector3f v_vec = eye_pos - point;
+        Eigen::Vector3f n_vec = normal;
+        float r_2 = std::pow(l_vect.x(), 2) + std::pow(l_vect.y(), 2) + std::pow(l_vect.z(), 2);
+        l_vect.normalize();
+        v_vec.normalize();
+        n_vec.normalize();
 
+        //镜面高光
+        Eigen::Vector3f h_light = l_vect + v_vec;
+        h_light.normalize();
+        //cwiseproduct 是将两个向量的每个项单独相乘，如k*b=（kxbx,kyby,kzbz），因为环境光，漫反射系数是分别作用于每个分量上的，
+        Eigen::Vector3f specular = ks.cwiseProduct(light.intensity / r_2) * std::pow(std::max(0.0f, n_vec.dot(h_light)), p);
+        //漫反射
+        Eigen::Vector3f diffuse = kd.cwiseProduct(light.intensity / r_2) * std::max(0.0f, l_vect.dot(n_vec));
+        //环境光
+        Eigen::Vector3f ambient = ka.cwiseProduct(amb_light_intensity);
+
+        result_color += specular + diffuse + ambient;
 
     }
 
